334c535305f38a2268124db3b31bff4d924de1a2
Nelson Romaine <nelson@romaine.life>
2026-02-22 01:45:37 -0800

    moving

---
+++ tofu/provider/shared_providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 3.0"
+    }
+    auth0 = {
+      source  = "auth0/auth0"
+      version = "~> 1.40.0"
+    }
+    spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "~> 1.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
